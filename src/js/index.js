7098d69125a176548247ca32ac5bb2aa0da4cd91
saviita <taatt2000@gmail.com>
2025-02-12 12:19:56 +0100

    terminado

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,19 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+const menuIconElement = document.getElementById('menu-icon')
+const menuElement = document.getElementById('menu-nav')
+
+const showMenu = () => {
+    if(menuElement.classList.contains('menu__hide')) {
+        menuIconElement.src = './assets/images/icon-menu-close.svg'
+        menuElement.classList.remove('menu__hide')
+        document.body.classList.add('menu-open')
+    } else {
+        menuIconElement.src = './assets/images/icon-menu.svg'
+        menuElement.classList.add('menu__hide')
+        document.body.classList.remove('menu-open')
+    }
+}
+
+menuIconElement.addEventListener('click', showMenu)
